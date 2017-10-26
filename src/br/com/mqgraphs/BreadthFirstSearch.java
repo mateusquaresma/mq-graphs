eb0e01a78983b6b7d7ac3fa8c256ff5cb0c612ae
Mateus Quaresma <mateus.quaresma@protonmail.com>
2017-10-25 22:48:01 -0200

    deteccao de ciclos na bfs

--- src/br/com/mqgraphs/BreadthFirstSearch.java
+++ src/br/com/mqgraphs/BreadthFirstSearch.java
@@ -7,7 +7,13 @@ public class BreadthFirstSearch {
 
     private boolean[] marked;
 
+    private int[] order;
+
+    private int[] parent;
+
     private Queue<Integer> queue;
+
+    private boolean cycle;
 
     public BreadthFirstSearch(Graph graph, int source) {
 
@@ -15,26 +21,49 @@ public class BreadthFirstSearch {
 
         queue = new PriorityQueue<>();
 
+        order = new int[graph.V()];
+
+        parent = new int[graph.V()];
+        for (int i = 0; i < parent.length; i++) parent[i] = -1;
+
         bfs(graph, source);
     }
 
     private void bfs(Graph graph, int source) {
+        int count = 0;
 
         queue.offer(source);
 
         while (!queue.isEmpty()) {
             Integer vertex = queue.poll();
             marked[vertex] = true;
-            for (Integer a : graph.adj(vertex))
-                if (!marked[a]) queue.offer(a);
+            order[vertex] = count++;
+
+            for (Integer a : graph.adj(vertex)) {
+
+                if (!marked[a]){
+                    parent[a] = vertex;
+                    queue.offer(a);
+                }
 
 
+                // Aplico a mesma técnica da busca em profundidade para detectar o ciclo
+                cycle = marked[a] && parent[vertex] != a;
+            }
         }
 
 
     }
 
-    public boolean marked(int vertex){
+    public boolean marked(int vertex) {
         return marked[vertex];
+    }
+
+    public int order(int vertex) {
+        return order[vertex];
+    }
+
+    public boolean hasCycle() {
+        return cycle;
     }
 }
--- src/br/com/mqgraphs/Main.java
+++ src/br/com/mqgraphs/Main.java
@@ -8,43 +8,43 @@ public class Main {
         Graph graph = new Graph(13);
 
 
-//        graph.addEdge(0,1);
-//        graph.addEdge(0,2);
-//        graph.addEdge(2,3);
-//        graph.addEdge(3,1);
+        graph.addEdge(0,1);
+        graph.addEdge(0,2);
+        graph.addEdge(2,3);
+        graph.addEdge(3,1);
 
 
-        graph.addEdge(4, 3);
-        graph.addEdge(9, 12);
-        graph.addEdge(6, 4);
-        graph.addEdge(0, 5);
-        graph.addEdge(0, 1);
-        graph.addEdge(5, 4);
-        graph.addEdge(0, 2);
-        graph.addEdge(11, 12);
-        graph.addEdge(9, 10);
-        graph.addEdge(0, 6);
-        graph.addEdge(7, 8);
-        graph.addEdge(9, 11);
-        graph.addEdge(5, 3);
-        graph.addEdge(6, 7);
-        graph.addEdge(8, 10);
+//        graph.addEdge(4, 3);
+//        graph.addEdge(9, 12);
+//        graph.addEdge(6, 4);
+//        graph.addEdge(0, 5);
+//        graph.addEdge(0, 1);
+//        graph.addEdge(5, 4);
+//        graph.addEdge(0, 2);
+//        graph.addEdge(11, 12);
+//        graph.addEdge(9, 10);
+//        graph.addEdge(0, 6);
+//        graph.addEdge(7, 8);
+//        graph.addEdge(9, 11);
+//        graph.addEdge(5, 3);
+//        graph.addEdge(6, 7);
+//        graph.addEdge(8, 10);
 
-        DepthFirstSearch search = new DepthFirstSearch(graph, 0);
+        DepthFirstSearch depthFirstSearch = new DepthFirstSearch(graph, 0);
 
         for (int v = 0; v < graph.V(); v++) {
-            if (search.marked(v))
+            if (depthFirstSearch.marked(v))
                 System.out.print(v + " ");
         }
 
         System.out.println();
 
-        if (search.count() != graph.V())
+        if (depthFirstSearch.count() != graph.V())
             System.out.print("NOT ");
 
         System.out.println("connected");
 
-        System.out.println("Has Cycle = " + search.hasCycle());
+        System.out.println("DFS Has Cycle? " + depthFirstSearch.hasCycle());
 
         TopologicalSort sort = new TopologicalSort(graph, 0);
 
@@ -54,17 +54,14 @@ public class Main {
 
         System.out.println();
 
-        BreadthFirstSearch bsearch = new BreadthFirstSearch(graph, 0);
+        BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch(graph, 0);
+
+        System.out.println("BFS Has Cycle? " + breadthFirstSearch.hasCycle());
 
         for (int v = 0; v < graph.V(); v++) {
-            if (bsearch.marked(v))
-                System.out.print(v + " ");
+            if (breadthFirstSearch.marked(v))
+                System.out.print(v + "(" + breadthFirstSearch.order(v) + ") ");
         }
-
-
-
-
-
     }
 
 }
